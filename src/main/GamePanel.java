66b7967ea42f2ad85667496886f61059a8830c7b
Domisalej6921 <dominickgeorge21@gmail.com>
2024-05-22 17:30:56 +0100

    Altered the graphic rendering logic to be more efficient, added more usable game tiles and inserted a new world map for the game.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -109,8 +109,13 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
-
         Graphics2D g2 = (Graphics2D)g;
+
+        //DEBUG
+        long drawStart = 0;
+        if(keyH.checkDrawTime) {
+            drawStart = System.nanoTime();
+        }
 
         //Tile
         tileM.draw(g2);
@@ -127,6 +132,15 @@ public class GamePanel extends JPanel implements Runnable{
 
         //UI
         ui.draw(g2);
+
+        //DEBUG
+        if(keyH.checkDrawTime) {
+            long drawEnd = System.nanoTime();
+            long passed = drawEnd - drawStart;
+            g2.setColor(Color.white);
+            g2.drawString("Draw Time: " + passed + " ns", 10, 400);
+            System.out.println("Draw Time: " + passed + " ns");
+        }
 
         g2.dispose();
     }
--- src/objects/SuperObject.java
+++ src/objects/SuperObject.java
@@ -1,6 +1,7 @@
 package objects;
 
 import main.GamePanel;
+import main.UtilityTool;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,13 +15,14 @@ public class SuperObject {
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
     public int solidAreaDefaultX = 0;
     public int solidAreaDefaultY = 0;
+    UtilityTool uTool = new UtilityTool();
 
     public void draw(Graphics2D g2, GamePanel gp) {
         int screenX = worldX - gp.player.worldX + gp.player.screenX;
         int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
         if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && worldX - gp.tileSize < gp.player.worldX + gp.player.screenX && worldY + gp.tileSize > gp.player.worldY - gp.player.screenY && worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
-            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+            g2.drawImage(image, screenX, screenY, null);
         }
     }
 }
