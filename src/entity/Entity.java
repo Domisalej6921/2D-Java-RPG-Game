234fc211f0f6ee13560d429e54156ff78c78c59a
Domisalej6921 <dominickgeorge21@gmail.com>
2024-05-24 12:45:26 +0100

    Tweaked the monsters ai to react to damage, added a health bar above monsters, added battle sound effects, and added monster dying animations.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -25,6 +25,9 @@ public class Entity {
     public int actionLockCounter = 0;
     public int invincibleCounter = 0;
     int dialogueCounter = 0;
+    int dyingCounter = 0;
+    int hpBarCounter = 0;
+
 
     //STATE
     public int worldX, worldY;
@@ -33,6 +36,9 @@ public class Entity {
     public boolean collisionOn = false;
     public boolean invincible = false;
     public boolean attacking = false;
+    public boolean alive = true;
+    public boolean dying = false;
+    boolean hpBarOn = false;
 
     //CHARACTER ATTRIBUTES
     public String name;
@@ -47,6 +53,9 @@ public class Entity {
 
     public void setAction() {
 
+    }
+
+    public void damageReaction() {
     }
 
     public void speak() {
@@ -97,6 +106,7 @@ public class Entity {
         if(this.type == 2 && contactPlayer) {
             if(!gp.player.invincible) {
                 //we give damage
+                gp.playSE(6);
                 gp.player.life -= 1;
                 gp.player.invincible = true;
             }
@@ -183,14 +193,79 @@ public class Entity {
                     break;
             }
 
+            //MONSTER HP BAR
+            if(type == 2 && hpBarOn) {
+
+                double oneScale = (double)gp.tileSize / maxLife;
+                double hpBarValue = oneScale * life;
+
+                g2.setColor(new Color(35, 35, 35));
+                g2.fillRect(screenX - 1, screenY - 16, gp.tileSize + 2, 12);
+
+                g2.setColor(new Color(255, 0, 30));
+                g2.fillRect(screenX, screenY - 15, (int) hpBarValue, 10);
+
+                hpBarCounter++;
+
+                if(hpBarCounter > 600) {
+                    hpBarOn = false;
+                    hpBarCounter = 0;
+                }
+            }
+
             if(invincible) {
-                g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f));
+                hpBarOn = true;
+                hpBarCounter = 0;
+                changeAlpha(g2, 0.4f);
+            }
+            if(dying) {
+                dyingAnimation(g2);
             }
 
             g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+            changeAlpha(g2, 1f);
         }
+    }
+
+    public void dyingAnimation(Graphics2D g2) {
+        dyingCounter++;
+
+        int i = 5;
+
+        if(dyingCounter <= i) {
+            changeAlpha(g2, 0f);
+        }
+        if(dyingCounter > i && dyingCounter <= i*2) {
+            changeAlpha(g2, 1f);
+        }
+        if(dyingCounter > i*2 && dyingCounter <= i*3) {
+            changeAlpha(g2, 0f);
+        }
+        if(dyingCounter > i*3 && dyingCounter <= i*4) {
+            changeAlpha(g2, 1f);
+        }
+        if(dyingCounter > i*4 && dyingCounter <= i*5) {
+            changeAlpha(g2, 0f);
+        }
+        if(dyingCounter > i*5 && dyingCounter <= i*6) {
+            changeAlpha(g2, 1f);
+        }
+        if(dyingCounter > i*6 && dyingCounter <= i*7) {
+            changeAlpha(g2, 0f);
+        }
+        if(dyingCounter > i*7 && dyingCounter <= i*8) {
+            changeAlpha(g2, 1f);
+        }
+        if(dyingCounter > i*8) {
+            dying = false;
+            dyingCounter = 0;
+            alive = false;
+        }
+    }
+
+    public void changeAlpha(Graphics2D g2, float alpha) {
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
     }
 
     public BufferedImage setup(String imagePath, int width, int height) {
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -223,6 +223,7 @@ public class Player extends Entity{
                 gp.gameState = gp.dialogueState;
                 gp.npc[i].speak();
             } else {
+                gp.playSE(7);
                 attacking = true;
             }
         }
@@ -233,6 +234,7 @@ public class Player extends Entity{
         if (monsterIndex != 999) {
 
             if(invincible == false) {
+                gp.playSE(6);
                 life -= 1;
                 invincible = true;
             }
@@ -243,11 +245,13 @@ public class Player extends Entity{
 
         if (i != 999) {
             if(!gp.monster[i].invincible) {
+                gp.playSE(5);
                 gp.monster[i].life -= 1;
                 gp.monster[i].invincible = true;
+                gp.monster[i].damageReaction();
 
                 if(gp.monster[i].life <= 0) {
-                    gp.monster[i] = null;
+                    gp.monster[i].dying = true;
                 }
             }
         }
